5a55624daea0ffde7d2f1901b7175900d0b78bb8
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-04-25 13:37:06 +0530

    Update receiver.c

--- receiver.c
+++ receiver.c
@@ -1,1 +1,9 @@
 #include "dataCollection.h"
+
+void readData(float* temperature, float* stateOfCharge, float* chargeRate, int noOfSamples){
+  for(int i = 0; i < noOfSamples; i++){
+     scanf("%f", &temperature[i]);
+     scanf("%f", &stateOfCharge[i]);
+     scanf("%f", &chargeRate[i]);
+  }
+}
