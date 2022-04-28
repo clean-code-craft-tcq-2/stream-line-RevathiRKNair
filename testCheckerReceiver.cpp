0535af7282564a11dc383f6d71e3a378bf426f7b
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-04-28 23:28:17 +0530

    Update testCheckerReceiver.cpp

--- testCheckerReceiver.cpp
+++ testCheckerReceiver.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "test/catch.hpp"
-#include "receiver.h"
+#include "dataReceiver.h"
 #include <string.h>
 #include <stdio.h>
 
