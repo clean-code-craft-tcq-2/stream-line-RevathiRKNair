e18c7580e92d89fa8903d0ebd2aba863c4e0bd7e
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-04-28 22:01:11 +0530

    Create receiver.h

---
+++ receiver.h
@@ -0,0 +1,5 @@
+void receiveData(float* currentReading, float* voltageReading, int noOfSamples);
+float findMin(float* input, int streamSize);
+float findMax(float* input, int streamSize);
+float ComputeAvg(float* input, int streamSize);
+void minMaxValues(float* currentReading, float* voltageReading, int streamSize);
