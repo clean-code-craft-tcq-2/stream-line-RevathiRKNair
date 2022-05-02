ed9b21f7a1eeceb258b8f32cde5bbd80e045bd17
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-05-02 10:20:35 +0530

    Update dataReceiver.h

--- dataReceiver.h
+++ dataReceiver.h
@@ -2,5 +2,6 @@
 void receiveData(float* currentReading, float* voltageReading, int noOfSamples);
 float findMin(float* input, int streamSize);
 float findMax(float* input, int streamSize);
-float ComputeAvg(float* input, int streamSize);
+float computeAvg(float* input, int streamSize);
+float computeSMA(float *input, int winSize);
 void minMaxValues(float* currentReading, float* voltageReading, int streamSize);
